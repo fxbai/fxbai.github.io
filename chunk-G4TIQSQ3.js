972e9c76ddda479b6cf3cee648ef98053930ba56
CubingTurtle <franklin.bai@gmail.com>
2025-09-24 00:30:17 -0400

    Auto-generated commit

---
+++ chunk-G4TIQSQ3.js
@@ -0,0 +1,1 @@
+import{La as o,Ma as a,Qa as c,ua as n}from"./chunk-HXN2W24L.js";var s=class e{static \u0275fac=function(t){return new(t||e)};static \u0275cmp=n({type:e,selectors:[["app-contact"]],decls:2,vars:0,template:function(t,p){t&1&&(o(0,"p"),c(1,"contact works!"),a())},encapsulation:2})};export{s as Contact};
